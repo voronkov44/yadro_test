234510ba22ad571c341d921eab78fdfc768e9f34
voronkov44 <123954369+voronkov44@users.noreply.github.com>
2024-05-10 02:52:02 +0300

    добавление закрытия столов в конце рабочего дня

--- main.go
+++ main.go
@@ -5,16 +5,10 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 )
-
-type Event struct {
-	Time   string
-	ID     int
-	Name   string
-	Number int
-}
 
 type Client struct {
 	Name      string
@@ -23,35 +17,9 @@ type Client struct {
 	Departure string
 }
 
-func formatTime(time string) string {
-	return time
-}
-
-// Функция для конвертации времени в формат int
-func convertTimeToInt(timeStr string) int {
-	var timeInt int
-	fmt.Sscanf(timeStr, "%d", &timeInt)
-	return timeInt
-}
-
-// Функция для получения времени, на которое стол был занят
-func getOccupiedTime(clients map[string]Client, table int) int {
-	var totalOccupiedTime int
-	for _, client := range clients {
-		if client.Table == table && client.Departure != "" {
-			arrivalTime := convertTimeToInt(client.Arrival)
-			departureTime := convertTimeToInt(client.Departure)
-			duration := (departureTime - arrivalTime + 100) / 100
-			totalOccupiedTime += duration
-		}
-	}
-	return totalOccupiedTime
-}
-
 func main() {
 	var tables int
 	var openTime, closeTime string
-	var hourCost int
 	var clients = make(map[string]Client)
 	var waitList []string
 
@@ -71,21 +39,19 @@ func main() {
 			openTime = line[0]
 			closeTime = line[1]
 			fmt.Println(openTime)
-		} else if len(line) == 1 {
-			hourCost, _ = strconv.Atoi(line[0])
-		} else {
+		} else if len(line) >= 3 {
 			eventTime := line[0]
 			eventID, _ := strconv.Atoi(line[1])
-			eventName := line[len(line)-1]
+			eventName := strings.Join(line[2:], " ")
 
 			switch eventID {
 			case 1:
 				if _, ok := clients[eventName]; ok {
-					fmt.Println(eventTime, "YouShallNotPass")
+					fmt.Println(eventTime, 13, "YouShallNotPass")
 					continue
 				}
 				if eventTime < openTime || eventTime > closeTime {
-					fmt.Println(eventTime, "NotOpenYet")
+					fmt.Println(eventTime, 13, "NotOpenYet")
 					continue
 				}
 				clients[eventName] = Client{Name: eventName, Arrival: eventTime}
@@ -93,15 +59,15 @@ func main() {
 			case 2:
 				client, ok := clients[eventName]
 				if !ok {
-					fmt.Println(eventTime, "ClientUnknown")
+					fmt.Println(eventTime, 13, "ClientUnknown")
 					continue
 				}
 				table, _ := strconv.Atoi(line[3])
-				if client.Table == table {
+				if client.Table != 0 && client.Table == table {
 					continue
 				}
 				if client.Table != 0 {
-					fmt.Println(eventTime, "PlaceIsBusy")
+					fmt.Println(eventTime, 13, "PlaceIsBusy")
 					continue
 				}
 				client.Table = table
@@ -109,7 +75,7 @@ func main() {
 				fmt.Println(eventTime, eventID, eventName, table)
 			case 3:
 				if len(clients) < tables {
-					fmt.Println(eventTime, "ICanWaitNoLonger!")
+					fmt.Println(eventTime, 13, "ICanWaitNoLonger!")
 					continue
 				}
 				waitList = append(waitList, eventName)
@@ -117,9 +83,10 @@ func main() {
 			case 4:
 				client, ok := clients[eventName]
 				if !ok {
-					fmt.Println(eventTime, "ClientUnknown")
+					fmt.Println(eventTime, 13, "ClientUnknown")
 					continue
 				}
+
 				client.Departure = eventTime
 				delete(clients, eventName)
 				fmt.Println(eventTime, eventID, eventName)
@@ -135,20 +102,14 @@ func main() {
 		}
 	}
 
-	for i := 1; i <= tables; i++ {
-		var revenue, timeOccupied int
-		for _, client := range clients {
-			if client.Table == i && client.Departure != "" {
-				arrivalMinute, _ := strconv.Atoi(strings.Replace(client.Arrival, ":", "", 1))
-				departureMinute, _ := strconv.Atoi(strings.Replace(client.Departure, ":", "", 1))
-				duration := (departureMinute - arrivalMinute + 59) / 60
-				revenue += duration * hourCost
-				timeOccupied += duration
-			}
-		}
-		if timeOccupied > 0 {
-			fmt.Printf("%d %d %02d:%02d\n", i, revenue, timeOccupied/60, timeOccupied%60)
-		}
+	var remainingClients []string
+	for clientName := range clients {
+		remainingClients = append(remainingClients, clientName)
 	}
+	sort.Strings(remainingClients)
+	for _, clientName := range remainingClients {
+		fmt.Printf("%s 11 %s\n", closeTime, clientName)
+	}
+
 	fmt.Println(closeTime)
 }
